5b8e643d2b65771c603a5373832d3a963073f972
MalbBros <66015024+Lendre-Hockage@users.noreply.github.com>
2020-12-25 13:14:55 +0300

    Добавлены тесты

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringTest {
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals("Eguauwgwug4uiooiwu", "HelloWorld!", Main.retString());
+    }
+}
+
