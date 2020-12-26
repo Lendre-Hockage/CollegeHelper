8c0bbdf5f4ad201d8e9a6875b4cbb8bf6fd2cc64
Mallowina <2003ksenia@mail.ru>
2020-12-26 12:34:57 +0300

    add javafx in maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
-public class Main {
+import java.lang.String;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!");
+        Application.launch(args);
     }
+
+    @Override
+    public void start(Stage stage) {
+        Button btn1 = new Button("OK");
+        Label lbl1 = new Label("Hi!");
+        Pane root = new Pane();
+        root.getChildren().addAll(btn1, lbl1);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Main menu"); // установка заголовка
+        stage.setWidth(1000);
+        stage.setHeight(450);
+        stage.show();                   // отображение окна на экране
+    }
+
     public static String retString(){
         return "HelloWorld!";
     }
