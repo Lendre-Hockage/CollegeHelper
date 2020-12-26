25276704b92da6bc4d13b64a711e691b3110e6bf
Mallowina <2003ksenia@mail.ru>
2020-12-26 21:28:25 +0300

    add base Authorization

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.lang.String;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
@@ -14,14 +17,21 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         Button btn1 = new Button("OK");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Authorization.input();
+            }
+        });
         Label lbl1 = new Label("Hi!");
         Pane root = new Pane();
         root.getChildren().addAll(btn1, lbl1);
         Scene scene = new Scene(root);
+
         stage.setScene(scene);
         stage.setTitle("Main menu"); // установка заголовка
         stage.setWidth(1000);
-        stage.setHeight(450);
+        stage.setHeight(650);
         stage.show();                   // отображение окна на экране
     }
 
